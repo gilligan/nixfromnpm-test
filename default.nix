065e42457ae209dcdf8f764857fbfa39f5e11f66
Rok Garbas <rok@garbas.si>
2016-02-17 22:25:59 +0100

    call imported nixpkgs

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { nodejs ? pkgs.nodejs-4_x
 , npm3 ? true
-, pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/885acea1dd82b0354ff2b6bcf1268b3031cf93df.tar.gz") 
+, pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/885acea1dd82b0354ff2b6bcf1268b3031cf93df.tar.gz") {}
 }:
 
 let
-    lib = import ./test { inherit pkgs npm3 nodejs; };
+  lib = import ./test { inherit pkgs npm3 nodejs; };
 in lib.callPackage ./project.nix {}
--- test/default.nix
+++ test/default.nix
@@ -1,9 +1,9 @@
 { nodejs ? pkgs.nodejs-4_x, npm3 ? true, pkgs ? import <nixpkgs> {} }:
 let
-    nodeLib = import ./nodeLib {
-      inherit pkgs npm3 nodejs;
-      self = nodeLib;
-    };
-    in nodeLib.generatePackages {
-      nodePackagesPath = ./nodePackages;
-    }
+  nodeLib = import ./nodeLib {
+    inherit pkgs npm3 nodejs;
+    self = nodeLib;
+  };
+in nodeLib.generatePackages {
+  nodePackagesPath = ./nodePackages;
+}
