4c502fcc0239e9c5a8c8b1220ff1c6ff3ff7e053
Rok Garbas <rok@garbas.si>
2016-02-17 22:16:36 +0100

    pinning to specific nixpkgs version and using relative path

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
-{ nodejs ? pkgs.nodejs-4_x, npm3 ? true, pkgs ? import <nixpkgs> {} }:
+{ nodejs ? pkgs.nodejs-4_x
+, npm3 ? true
+, pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/885acea1dd82b0354ff2b6bcf1268b3031cf93df.tar.gz") 
+}:
+
 let
-    lib = import /Users/tpflug/Development/nix/nodejs-testing/test {
-      inherit pkgs npm3 nodejs;
-    };
-    in lib.callPackage ./project.nix {}
+    lib = import ./test { inherit pkgs npm3 nodejs; };
+in lib.callPackage ./project.nix {}
